d091075e4487ee35659f0a57eb59f0568401caef
FilippiMnz <imperilkill12@gmail.com>
2025-03-07 23:26:25 -0300

    login

--- SistemaBanco/main.py
+++ SistemaBanco/main.py
@@ -5,3 +5,18 @@ print(cliente1.imprima())
 
 cliente1.setNome("Hominho")
 print(cliente1.imprima())
+
+
+print("Welcome to the los pollos hermanos")
+print("Select your option: ")
+print(
+    "1- Cadastrar"
+    "\n2- Fazer Login"
+    )
+opcao = input(int())
+
+if opcao == 1:
+    print("fazer Cadastro")
+elif opcao == 2:
+    print("fazer Login")
+
