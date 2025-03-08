931f0a695536aff1a82ac6594190634bd0e7acb6
FilippiMnz <imperilkill12@gmail.com>
2025-03-08 11:23:03 -0300

    idConta

--- SistemaBanco/Cadastro.py
+++ SistemaBanco/Cadastro.py
@@ -1,8 +1,8 @@
 from cliente import Cliente
 
 class Usuario(Cliente):
-    def __init__(self, nome, cpf, RG, endereco, dataNascimento, email, senha):
-        super().__init__(nome, cpf, RG, endereco, dataNascimento)
+    def __init__(self, nome, cpf, RG, endereco, dataNascimento, email, senha, idConta = None):
+        super().__init__(nome, cpf, RG, endereco, dataNascimento, idConta = None)
         self.__email = email
         self.__senha = senha
         self.__contas = []
@@ -25,14 +25,20 @@ class Usuario(Cliente):
     def adicionarConta(self, conta):
         if isinstance(conta, Cliente):
             self.__contas.append(conta)
+            return True
     
     def removerConta(self, cpf):
         for conta in self.__contas:
             if conta.getCPF() == cpf:
                 self.__contas.remove(conta)
                 return True
-            return False
+        return False
         
     def Listar_contas(self):
         for conta in self.__contas:
             conta.imprima()
+    
+    def ModificarConta(self, cpf):
+        for conta in self.__contas:
+            if conta.getCPF() == cpf:
+                return f"Olá {self.__nome}, Conta: {self.__idConta}"
--- SistemaBanco/cliente.py
+++ SistemaBanco/cliente.py
@@ -1,10 +1,13 @@
+import random 
+from random import *
 class Cliente:
-    def __init__(self, nome, cpf, RG, endereco, dataNascimento):
+    def __init__(self, nome, cpf, RG, endereco, dataNascimento, idConta = None):
         self.__nome = nome
         self.__cpf = cpf
         self.__rg = RG
         self.__endereco = endereco
         self.__dataNascimento = dataNascimento
+        self.__idConta = idConta if idConta else random.randint(1, 100000)
 
     def getNome(self):
         return self.__nome
@@ -21,6 +24,9 @@ class Cliente:
     def getDataNasci(self):
         return self.__dataNascimento
     
+    def getId(self):
+        return self.__idConta
+    
     def setNome(self, newNome):
         self.__nome = newNome
 
@@ -35,6 +41,9 @@ class Cliente:
 
     def setDataNascimento(self, newDataNascimento):
         self.__dataNascimento = newDataNascimento
+
+    def setId(self, newId):
+        self.__idConta = newId
     
     def imprima(self):
-        return f"Cliente {self.__nome} CPF: {self.__cpf}, Endereço: {self.__endereco}, Data nascimento: {self.__dataNascimento}, RG: {self.__rg}"
+        return f"Cliente {self.__nome} CPF: {self.__cpf}, Endereço: {self.__endereco}, Data nascimento: {self.__dataNascimento}, RG: {self.__rg}, conta: {self.__idConta}"
