c21b58d48175602c70fc3e2694f3cda5e3e183dd
FilippiMnz <imperilkill12@gmail.com>
2025-03-09 19:30:22 -0300

    adm

---
+++ SistemaBanco/adm.py
@@ -0,0 +1,12 @@
+from Cadastro import *
+
+class ADM(Usuario):
+    def __init__(self, nome, cpf, RG, endereco, dataNascimento, email, senha, cargo, idConta = None):
+        super().__init__(nome, cpf, RG, endereco, dataNascimento, email, senha, idConta)
+        self.__cargo = cargo
+    
+    def getCargo(self):
+        return self.__cargo
+    
+    def setCargo(self, newCargo):
+        self.__cargo = newCargo
