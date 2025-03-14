e79425f28b6f793b220fdb0769a1468a9dffaacf
FilippiMnz <imperilkill12@gmail.com>
2025-03-13 22:15:00 -0300

    3

--- SistemaBanco/adm.py
+++ SistemaBanco/adm.py
@@ -31,9 +31,13 @@ class ADM(Usuario):
                     print("usuario Encontrado")
                     print(f"Usuario: {self.__nome}, Conta: {self.__idConta}, Saldo: {self.getSaldo()}")
                     return True
-            if option == 2:
+            elif option == 2:
                 print("insira o CPF do usuario que deseja remover")
                 cpfUser = input()
                 if self.getCPF == cpfUser:
                     return True
-                
+            
+            elif option == 3:
+                print("saindo")
+                return True 
+            
